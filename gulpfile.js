89d0aedbc8f1923395da856f8b063677d2151704
Aleksandr Guryanov <aleksandr.guryanov@gmail.com>
2019-12-04 15:58:20 -0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -25,25 +25,31 @@ const pug = require('gulp-pug');
 // patch
 var path = {
   build: {
-    html: 'build',
-    js: 'build/js',
-    style: 'build/css',
-    img: 'build/img',
-    icons: 'build/img/icons',
-    fonts: 'build/fonts',
-    misc: 'build/'
+    html: './build',
+    js: './build/js',
+    style: './build/css',
+    img: './build/img',
+    icons: './build/img/icons',
+    fonts: './build/fonts',
+    misc: './build/'
   },
   src: {
-    html: 'src/*.pug, src/layout/**/',
-    js: 'src/common/js/main.js',
-    style: 'src/common/scss/*.scss',
-    img: 'src/common/assets/images/**/*.*',
-    icons: 'src/common/assets/icons/*.svg',
-    fonts: 'src/common/fonts/*.*',
-    misc: 'src/mics/*.*',
+    html: './src/*.pug, ./src/layout/**/',
+    js: './src/common/js/main.js',
+    style: './src/common/scss/main.scss',
+    img: './src/common/assets/images/**/*.*',
+    icons: './src/common/assets/icons/*.svg',
+    fonts: './src/common/fonts/*.*',
+    misc: './src/mics/*.*',
   },
   watch: {
-    html: 'build/*.html',
+    html: './src/*.pug, ./src/layout/**/',
+    js: './src/common/js/main.js',
+    style: './src/common/scss/**/*.scss, ./src/common/scss/*.scss, ./src/components/**/*.scss, ./src/components/**/**/*.scss',
+    img: './src/common/assets/images/**/*.*',
+    icons: './src/common/assets/icons/*.svg',
+    fonts: './src/common/fonts/*.*',
+    misc: './src/mics/*.*',
   },
 }
 
@@ -56,17 +62,28 @@ function html() {
     .pipe(dest(path.build.html))
 }
 
+// Compile pug files into HTML
+function style() {
+  return src([path.src.style])
+    .pipe(sass({
+      pretty: true
+    }))
+    .pipe(dest(path.build.style))
+}
+
 // Serve and watch sass/pug files for changes
 function watchAndServe() {
   browserSync.init({
-    server: 'build',
+    server: './build',
   })
 
-  watch([path.src.html], html)
-  watch(path.watch.html).on('change', browserSync.reload)
+  watch([path.watch.html], html)
+  watch([path.watch.style], style)
+  watch(path.build.html).on('change', browserSync.reload)
 }
 
 exports.html = html;
+exports.style = style;
 exports.watch = watchAndServe;
 
-exports.default = series(html, watchAndServe);
+exports.default = series(html, style, watchAndServe);
